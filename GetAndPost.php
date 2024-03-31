742dc0dcee585d34fae1c2911f95ec8b0d540e1c
Rajatbisht12 <500095841@stu.upes.ac.in>
2024-04-01 00:00:12 +0530

    php

---
+++ GetAndPost.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        echo '<h1 class="bg-blue-900">Hello-World</h1>';
+    ?>
+</body>
+</html>
